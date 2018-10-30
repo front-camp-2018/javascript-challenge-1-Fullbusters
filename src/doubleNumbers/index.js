67043ba13c44afced9b8541f81cc8155e80b8e36
Fullbusters <fullbuster0033@gmail.com>
2018-10-30 08:33:12 +0200

    did the tast doubleNum

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,13 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const stringnum = '' + num;
+  const lenght = stringnum.length;
+  const leftpath = stringnum.slice(0,lenght / 2);
+  const rightpath = stringnum.slice(lenght / 2, lenght);
+  if (leftpath === rightpath) {
+    return num;
+  } else {
+    return num * 2;
+  }
+  
 };
