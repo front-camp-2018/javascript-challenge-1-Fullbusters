ff0c593197c7fff7c390b320dfd7e3fa8c20b71a
Fullbusters <fullbuster0033@gmail.com>
2018-10-30 22:20:30 +0200

    Done assignments doubleNum and sortByWeight

--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,14 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+
+  arr.sort((first, second) => {
+    const firstAmount = [...first].reduce((sum, current) => sum + Number(current), 0);
+      
+    const secondAmount = [...second].reduce((sum, current) => sum + Number(current), 0);
+
+    return firstAmount - secondAmount;
+  });
+  return arr;
+ 
+  
 };
